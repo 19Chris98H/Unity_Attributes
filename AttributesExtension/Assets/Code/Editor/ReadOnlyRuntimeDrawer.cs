0593673cf2da3837822a8fff9b4b4cf48c43247d
Chris9898 <c.hoerath@gmx.net>
2023-05-27 21:59:55 +0200

    add read only attributes

---
+++ AttributesExtension/Assets/Code/Editor/ReadOnlyRuntimeDrawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyRuntimeAttribute))]
+public class ReadOnlyRuntimeDrawer : ReadOnlyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorApplication.isPlaying ? base.GetPropertyHeight(property, label) : 0;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (EditorApplication.isPlaying)
+            base.OnGUI(position, property, label);
+    }
+}
